94ae98b6e8b78c33e8a592a87c56b9748fa2e6a3
Paulo Marques <paulormarques21@gmail.com>
2021-11-10 13:15:30 -0300

    header: update

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 23:52:38 by proberto          #+#    #+#             */
-/*   Updated: 2021/10/28 21:35:42 by proberto         ###   ########.fr       */
+/*   Updated: 2021/11/10 12:26:10 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@
 # include "../src/libft/libft.h"
 
 # define TRUE 1
-# define FALSE 1
+# define FALSE 0
+# define SWAP 5
+# define PUSH 6
+# define ROTATE 7
+# define REVERSE 8
+# define INT_MAX 2147483647
+# define INT_MIN -2147483648
 
 typedef enum e_swap {
 	SA,
@@ -37,10 +43,13 @@ typedef enum e_reverse_rotate {
 }			t_reverse_rotate;
 
 typedef struct s_stack {
-	int	*stack;
+	int	*array;
 	int	size;
 	int	len;
 	int	top;
+	int	smaller;
+	int	larger;
+	int	sorted;
 }		t_stack;
 
 void	ft_swap(t_stack *stack_a, t_stack *stack_b, t_swap op);
@@ -49,5 +58,15 @@ void	ft_push_b(t_stack *stack_b, t_stack *stack_a);
 void	ft_rotate(t_stack *stack_a, t_stack *stack_b, t_rotate op);
 void	ft_reverse_rotate(t_stack *stack_a, t_stack *stack_b,
 			t_reverse_rotate op);
+int		ft_is_sorted(int *array, int i, int len);
+int		ft_get_smallest(int *array, int len, int i, int ref);
+int		ft_get_smallest_index(int *array, int i, int len);
+int		ft_get_biggest(int *array, int len, int i, int ref);
+int		ft_get_biggest_index(int *array, int i, int len);
+void	ft_sorting(t_stack *stack_a, t_stack *stack_b);
+void	ft_soft_sorting(t_stack *stack_a, t_stack *stack_b);
+void	ft_medium_sorting(t_stack *stack_a, t_stack *stack_b);
+void	ft_hard_sorting(t_stack *stack_a, t_stack *stack_b);
+void	ft_extremely_hard_sorting(t_stack *stack_a, t_stack *stack_b);
 
 #endif
